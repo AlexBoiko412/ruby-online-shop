3ae6ff3af0f664d20d4a3efb15bac4794a8dfda2
AlexBoiko412 <bojkosasa78@gmail.com>
2025-12-04 11:08:05 +0000

    Product Section

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,76 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 require "open-uri"
 
 Product.destroy_all
 Size.destroy_all
+ProductSize.destroy_all
+ActiveStorage::Attachment.destroy_all
+ActiveStorage::Blob.destroy_all
 
-(38..46).each { |s| Size.create!(eu_size: s) }
-
-products_data = [
-  { name: "Nike Air Max 270",      brand: "Nike",    price: 5499,  img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/6e0c6e1a-9b5d-4f0f-9e9e-8e5e8f8b8b8b/air-max-270-mens-shoes-KkLcSR.png" },
-  { name: "Adidas Ultraboost 23",  brand: "Adidas",  price: 7299,  img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/1f7b5c9e7a1d4e1b8c2aafd600e1c5e5_9366/Ultraboost_23_Shoes_Black_GY9353_01_standard.jpg" },
-  { name: "Puma RS-X Toys",        brand: "Puma",    price: 4299,  img: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/369579/01/sv01/fnd/EEA/fmt/png" },
-  { name: "New Balance 550",       brand: "New Balance", price: 5799, img: "https://nb.scene7.com/is/image/NB/bb550wt1_nb_02_i?$pdpflex2276$" },
-  { name: "Asics Gel-Kayano 30",   brand: "Asics",   price: 6799,  img: "https://asics.scene7.com/is/image/asics/1011B548_400_SR_RT_GLB?$SFCC-zoom$" },
-  { name: "Reebok Classic Leather",brand: "Reebok",  price: 3499,  img: "https://reebok.by/images/products/2023/11/100008491_1.jpg" },
-  { name: "Nike React Element 55", brand: "Nike",    price: 4899,  img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/u3d0m2f5q0d5r2b1n1j2/react-element-55-mens-shoes-6kL9k3.png" },
-  { name: "Adidas NMD_R1",         brand: "Adidas",  price: 5999,  img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/9d4b3e3b8f1f4e1b8c2aafd600e1c5e5_9366/NMD_R1_Shoes_Black_GZ9257_01_standard.jpg" },
+(38..46).each { |n| Size.create!(eu_size: n) }
+products = [
+  {
+    name: "Nike Air Max 270",
+    brand: "Nike",
+    price: 5499,
+    description: "Легендарна модель з максимальною амортизацією Air.",
+    photos: [
+      "https://images1.sportpursuit.info/media/catalog/product/cache/1/image/800x800/040ec09b1e35df139433887a97daa66f/N/i/Nike_Oct25-BV2523-100_1.webp",
+    ]
+  },
+  {
+    name: "Adidas Ultraboost 23",
+    brand: "Adidas",
+    price: 7299,
+    description: "Преміум-бігові кросівки з Boost-підошвою.",
+    photos: [
+      "https://intersport-de.imgdn.net/fsi/server?type=image&effects=Pad(cc,ffffff),Matte(FFFFFF)&width=1200&height=1200&source=marktplatz%2Fproduktiv%2F030%2FHQ6353%2FHQ6353_000_BILD01_20230729.jpg"
+    ]
+  },
+  {
+    name: "Puma RS-X Toys",
+    brand: "Puma",
+    price: 4299,
+    description: "Яскравий ретро-дизайн з сучасними технологіями.",
+    photos: [
+      "https://img01.ztat.net/article/spp-media-p1/9d191ee41f70479c8bf8d5211a6011dd/7cddc8aebced4cc7a0d407f8d730c159.jpg?imwidth=1800&filter=packshot"
+    ]
+  },
+  {
+    name: "New Balance 550 White/Green",
+    brand: "New Balance",
+    price: 5799,
+    description: "Культова модель 80-х у сучасному виконанні.",
+    photos: [
+      "https://nb.scene7.com/is/image/NB/bb550ptb_nb_02_i?$dw_detail_main_lg$&bgc=f1f1f1&layer=1&bgcolor=f1f1f1&blendMode=mult&scale=10&wid=1600&hei=1600",
+      "https://nb.scene7.com/is/image/NB/bb550ptb_nb_04_i?$dw_detail_main_lg$&bgc=f1f1f1&layer=1&bgcolor=f1f1f1&blendMode=mult&scale=10&wid=1600&hei=1600",
+      "https://nb.scene7.com/is/image/NB/bb550ptb_nb_07_i?$dw_detail_main_lg$&bgc=f1f1f1&layer=1&bgcolor=f1f1f1&blendMode=mult&scale=10&wid=1600&hei=1600"
+    ]
+  },
+  {
+    name: "Asics Gel-Kayano 30",
+    brand: "Asics",
+    price: 6799,
+    description: "Максимальна стабільність і амортизація для бігу.",
+    photos: [
+      "https://sportano.de/img/986c30c27a3d26a3ee16c136f92f4ff5/1/9/197968081872_20-jpg/laufschuhe-damen-new-balance-fuelcell-venym-calcium-1484291.jpg"
+    ]
+  }
 ]
 
-products_data.each do |data|
-  product = Product.create!(name: data[:name], brand: data[:brand], price: data[:price], description: "#{data[:brand]} #{data[:name]} — топова модель 2025 року!")
-
-  downloaded_image = URI.open(data[:img])
-  product.photo.attach(io: downloaded_image, filename: "#{data[:name].parameterize}.jpg", content_type: "image/jpeg")
-
-  Size.all.each { |size| product.sizes << size }
+products.each do |p|
+  product = Product.create!(
+    name:        p[:name],
+    brand:       p[:brand],
+    price:       p[:price],
+    description: p[:description],
+    photo_urls:  p[:photos].join("|||")
+  )
+  Size.all.each { |s| product.sizes << s }
 end
 
-User.create!(email: "admin@sportfoot.ua", password: "123456", password_confirmation: "123456") unless User.exists?
+User.find_or_create_by!(email: "admin@sportfoot.ua") do |u|
+  u.password = "123456"
+  u.password_confirmation = "123456"
+end
 
 puts "Створено #{Product.count} товарів!"
