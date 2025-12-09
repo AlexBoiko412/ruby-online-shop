22feef6f608484ecead91fdf0dbc7c00564c608d
AlexBoiko412 <bojkosasa78@gmail.com>
2025-12-09 12:10:34 +0000

    Filtration controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    @q = Product.ransack(params[:q])
+
+    sort = params[:sort] || "newest"
+    case sort
+    when "price_asc"
+      @products = @q.result(distinct: true).includes(:sizes).order(price: :asc)
+    when "price_desc"
+      @products = @q.result(distinct: true).includes(:sizes).order(price: :desc)
+    when "name"
+      @products = @q.result(distinct: true).includes(:sizes).order(name: :asc)
+    else
+      @products = @q.result(distinct: true).includes(:sizes).order(created_at: :desc)
+    end
   end
 end
